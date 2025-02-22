dcbf6baa4a169405add63bc8db1574ae92d055dd
Virlan Victor-Stefan <roxorvicecity@gmail.com>
2025-02-22 21:19:21 +0200

    Am citit fisierul text

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,9 @@
 import java.io.*;
 public class Main {
-    public static void main(String[] args){
-    
+    public static void main(String[] args) throws IOException{
+        FileReader fr=new FileReader("text.txt");
+        BufferedReader br=new BufferedReader(fr);
+        String line=br.readLine();
+        System.out.printf("%s",line);
     }
 }
