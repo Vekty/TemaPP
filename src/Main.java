8dc1bd55b7a5b3c1151cc7148a2fc5b37da83f58
Virlan Victor-Stefan <roxorvicecity@gmail.com>
2025-02-23 12:40:22 +0200

    Am adaugat ultima functionalitate

--- src/Main.java
+++ src/Main.java
@@ -3,20 +3,30 @@ public class Main {
     public static void main(String[] args) throws IOException{
         FileReader fr=new FileReader("text.txt");
         BufferedReader br=new BufferedReader(fr);
-        String line=br.readLine();
-        System.out.printf("%s\n",line);
+        StringBuilder sb=new StringBuilder();
+        String line;
+        while((line=br.readLine())!=null)
+        {
+            sb.append(line).append('\n');
+        }
+        String content=sb.toString();
+        System.out.printf("%s\n",content);
         InputStreamReader isr=new InputStreamReader(System.in);
         BufferedReader brr=new BufferedReader(isr);
         int number=Integer.parseInt(brr.readLine());
         switch(number)
         {
             case 0:
-                String upper=line.toUpperCase();
+                String upper=content.toUpperCase();
                 System.out.printf("%s",upper);
                 break;
             case 1:
-                String clear=line.replaceAll("[,.?!]","");
+                String clear=content.replaceAll("[,.?!]","");
                 System.out.printf("%s",clear);
+                break;
+            case 2:
+                String nospace=content.replaceAll(" {2,}"," ");
+                System.out.printf("%s",nospace);
                 break;
         }
     }
